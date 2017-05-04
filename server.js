e9741abdda43ab6a908290d70e2f46a9f6f3413c
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-05-04 15:03:29 +0200

    test

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+server.listen(80);
+io.on('connection', function(socket) {
+    console.log("user connected");
+    socket.emit('connect', "connected");
+});
 var fs = require('fs');
 var path = require("path");
 var bodyParser = require('body-parser');
@@ -15,13 +19,9 @@ var controllers = require('./controllers');
 
 controllers.set(app);
 
-io.on('connection', function(socket) {
-    console.log("user connected");
-    socket.emit('connected', "connected");
-});
+
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
     fs.createReadStream("./public/index.html").pipe(res);
 });
-app.listen(80);
