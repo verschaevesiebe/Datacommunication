1577bd93a4942e1656c3ead753915fb12f36cf35
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-05-04 14:50:29 +0200

    load order

--- server.js
+++ server.js
@@ -6,20 +6,19 @@ var io = require('socket.io')(server);
 var fs = require('fs');
 var path = require("path");
 var bodyParser = require('body-parser');
-io.on('connection', function(socket) {
-    console.log("user connected");
-    socket.emit('connected', "connected");
-});
-
-var controllers = require('./controllers');
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+var controllers = require('./controllers');
+
 controllers.set(app);
 
+io.on('connection', function(socket) {
+    console.log("user connected");
+    socket.emit('connected', "connected");
+});
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
