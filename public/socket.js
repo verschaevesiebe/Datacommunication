508cc5188667b28410af6e1b0bbd3053d908e071
verschaevesiebe <verschaevesiebe@hotmail.com>
2017-05-04 15:59:54 +0200

    Html added articles

--- public/socket.js
+++ public/socket.js
@@ -10,7 +10,7 @@ function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(51.5, -0.12),
         zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
